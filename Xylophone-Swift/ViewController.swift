c40ac46253c1151869c27fd9bdeca16f111ad983
Yamisagi <erencandan20@gmail.com>
2023-03-21 23:53:43 +0300

    Add IBAction func

--- Xylophone-Swift/ViewController.swift
+++ Xylophone-Swift/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print("Hello")
+    }
+    
 }
 
